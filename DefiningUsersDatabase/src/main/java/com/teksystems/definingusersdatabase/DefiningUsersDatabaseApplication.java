94028871aa3e84ceaaef26196041d6945fe2f5e3
Yashas R <yasr@BLR.ALLEGISINDIA.COM>
2026-02-16 21:35:48 +0530

    defining users

---
+++ DefiningUsersDatabase/src/main/java/com/teksystems/definingusersdatabase/DefiningUsersDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.teksystems.definingusersdatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DefiningUsersDatabaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DefiningUsersDatabaseApplication.class, args);
+    }
+
+}
---
+++ DefiningUsersDatabase/src/test/java/com/teksystems/definingusersdatabase/DefiningUsersDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teksystems.definingusersdatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DefiningUsersDatabaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
